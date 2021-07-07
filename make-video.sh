891d8728b9eae0b9398770103ce1cbe5e3f0d25e
ostoic <ostoic@uwindsor.ca>
2021-07-07 15:49:25 -0400

    Change output folder

--- make-video.sh
+++ make-video.sh
@@ -2,7 +2,7 @@
 
 if python3 add-timestamp.py $1 ; then
   #rm "$1"/eye-*.jpg
-  ffmpeg -framerate 1 -pattern_type glob -i "$1/eyets-*.jpg" "$1.mp4"
+  ffmpeg -framerate 1 -pattern_type glob -i "$1/eyets-*.jpg" "videos/$1.mp4"
   rm "$1"/eyets-*.jpg
 else
   echo "Error making video"
